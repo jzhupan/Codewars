9e08175f70c43ab1e52169b3411b1c5977eee7bb
jzhupan <jzhupan@gmail.com>
2023-03-24 14:14:41 -0400

    Completed 2 codewars challenges, Reversed Strings and converting string into a number.

--- app.js
+++ app.js
@@ -40,4 +40,60 @@ function digitize(n) {
 
 
 
+//Reversed Strings
+//Complete the solution so that it reverses the string passed into it.
 
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+  let array = []
+  for(let i =0 ; i < str.length; i++){
+    //console.log(str[i])
+    array.push(str[i])
+  }
+  array.reverse()
+  //console.log(array)
+  return array.join('')
+}
+
+
+
+
+//Converting string into a Number!
+/*
+Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+Description
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+Examples
+"1234" --> 1234
+"605"  --> 605
+"1405" --> 1405
+"-7" --> -7
+*/
+
+const stringToNumber = function(str){
+  // put your code here
+  
+  let strToNum = Math.floor(str)
+  //console.log(strToNum)
+  return strToNum
+}
+
+
+
+
+//Grasshopper - Summation
+
+/*
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example (Input -> Output):
+
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+*/
