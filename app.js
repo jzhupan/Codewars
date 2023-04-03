77d8a8b8a1b154297565d4331f5803922920f871
jzhupan <jzhupan@gmail.com>
2023-04-02 21:59:58 -0400

    Completed 'Keep Hydrated!' exercise.

--- app.js
+++ app.js
@@ -163,3 +163,30 @@ function getGrade (s1, s2, s3) {
       return 'F'
     }
 }
+
+
+//Keep Hydrated!
+/*
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+For example:
+
+time = 3 ----> litres = 1
+
+time = 6.7---> litres = 3
+
+time = 11.8--> litres = 5
+*/
+
+function litres(time) {
+  //console.log(time) //output 2 hours
+  
+  let litresPerHour = 0.5
+  let amountOfWaterNeeded = Math.floor(time* litresPerHour)
+  return amountOfWaterNeeded; 
+}
+
