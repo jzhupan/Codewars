5954b04f7c4bbb3a366d8322b6d73abf88280a93
jzhupan <jzhupan@gmail.com>
2023-05-05 16:37:54 -0400

    Practice and repeat, solved 6 challenges today.

--- practiceAndRepeat/app.js
+++ practiceAndRepeat/app.js
@@ -5,6 +5,87 @@ const stringToNumber = function(str){
     return number;
   }
 
+//You can't Code Under Pressure #1
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  let doubleNum = i * 2
+  return doubleNum;
+}
 
+//Convert boolean values to strings 'Yes' or 'No'.
+function boolToWord( bool ){
+  if(bool == true){
+    return 'Yes'
+  } else {
+    return 'No'
+  }
+}
 
+//MakeUpperCase
+function makeUpperCase(str) {
+  // Code here
+  let upperCase = str.toUpperCase()
+  return upperCase
+}
+
+//String repeat
+
+function repeatStr (n, s) {
+  let stringTimes= '';
+  for(let i = 0; i < n; i++){
+   stringTimes += s 
+  } 
+  console.log(stringTimes)
+  return stringTimes;
+}
+
+//Switch it Up!
+function switchItUp(number){
+  //Write your own Code!
+    switch(number) {
+        case 0 :
+        return 'Zero'
+        break;
+        case 1 :
+        return 'One'
+        break;
+        case 2 :
+        return 'Two'
+        break;
+        case 3 :
+        return 'Three'
+        break;
+        case 4 :
+        return 'Four'
+        break;
+        case 5 :
+        return 'Five'
+        break;
+        case 6 :
+        return 'Six'
+        break;
+        case 7 :
+        return 'Seven'
+        break;
+        case 8 :
+        return 'Eight'
+        break;
+        case 9 :
+        return 'Nine'
+        break;
+        default:
+        console.log('Enter a valid number between 0-9')
+    }
+  }
+
+//Twice as Old
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here
+  let sonsDoubleAge = sonYearsOld * 2
+  let yearsDifference = dadYearsOld - (sonsDoubleAge)
+  if(yearsDifference < 0){
+    return yearsDifference * -1
+  }
+  return yearsDifference
+}
 
